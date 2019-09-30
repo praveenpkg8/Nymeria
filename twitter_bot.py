d2c5c18b4f2275e89c96e70d73367c0eeb5a4db8
praveen <praveenpkg8@gmail.com>
2019-09-30 12:40:51 +0530

    made changes in retweet

--- twitter_bot.py
+++ twitter_bot.py
@@ -3,7 +3,8 @@ import json
 
 from urllib.parse import urlencode
 
-url="https://api.twitter.com/oauth2/token"
+url = "https://api.twitter.com/oauth2/token"
+retweet_url = "https://api.twitter.com/1.1/statuses/retweet/1177602818741374976"
 
 api_file = open("twitter_api_key.json", "r")
 data = json.load(api_file)
@@ -21,13 +22,25 @@ access_token = response_data.get("access_token")
 
 Authorization = "Bearer {}".format(access_token)
 headers = {"Authorization": Authorization}
-query_params = urlencode({ "q": "#100DaysOfCode -filter:retweets AND -filter:replies"})
+query_params = urlencode({"id": "1177602818741374976"})
 
-endpoint_url = "https://api.twitter.com/1.1/search/tweets.json"
-response = requests.get(url=endpoint_url,params=query_params,headers=headers)
-search_tweets = json.loads(response.text)
-with open("data.json", "w") as json_file:
-    json.dump(search_tweets,json_file)
+response = requests.post(
+    retweet_url,
+    params=query_params,
+    headers=headers,
+)
+print(response.text)
+
+
+# query_params = urlencode({ "q": "#100DaysOfCode -filter:retweets AND -filter:replies"})
+
+
+
+# endpoint_url = "https://api.twitter.com/1.1/search/tweets.json"
+# response = requests.get(url=endpoint_url,params=query_params,headers=headers)
+# search_tweets = json.loads(response.text)
+# with open("data.json", "w") as json_file:
+#     json.dump(search_tweets,json_file)
 # tweets_list = search_tweets.get('statuses')
 # for tweets in tweets_list:
 #     print(tweets.get("text"))
