040e37556ed05941b4d547ce62fea9aa2280469a
praveen <praveenpkg8@gmail.com>
2019-10-05 20:25:29 +0530

    restructuring

--- twitter_bot.py
+++ twitter_bot.py
@@ -17,35 +17,35 @@ r = requests.post(
     auth=(data.get('api_key'), data.get('api_secret_key')))
 
 
-tweet_id_data = open("data.json", "r")
-tweet_id = json.load(tweet_id_data)
-tweet_id_json = json.dump(tweet_id)
+# tweet_id_data = open("data.json", "r")
+# tweet_id = json.load(tweet_id_data)
+# tweet_id_json = json.dump(tweet_id)
 
 response_data = json.loads(r.text)
 access_token = response_data.get("access_token")
 
 Authorization = "Bearer {}".format(access_token)
 headers = {"Authorization": Authorization}
-query_params = urlencode({"id": "1177602818741374976"})
-
-response = requests.post(
-    retweet_url,
-    params=query_params,
-    headers=headers,
-)
-print(response.text)
-
-
-# query_params = urlencode({ "q": "#100DaysOfCode -filter:retweets AND -filter:replies"})
+# query_params = urlencode({"id": "1177602818741374976"})
+#
+# response = requests.post(
+#     retweet_url,
+#     params=query_params,
+#     headers=headers,
+# )
+# print(response.text)
 
 
+query_params = urlencode({ "q": "#100DaysOfCode -filter:retweets AND -filter:replies"})
 
-# endpoint_url = "https://api.twitter.com/1.1/search/tweets.json"
-# response = requests.get(url=endpoint_url,params=query_params,headers=headers)
-# search_tweets = json.loads(response.text)
-# with open("data.json", "w") as json_file:
-#     json.dump(search_tweets,json_file)
-# tweets_list = search_tweets.get('statuses')
-# for tweets in tweets_list:
-#     print(tweets.get("text"))
+
+
+endpoint_url = "https://api.twitter.com/1.1/search/tweets.json"
+response = requests.get(url=endpoint_url, params=query_params,headers=headers)
+search_tweets = json.loads(response.text)
+with open("data.json", "w") as json_file:
+    json.dump(search_tweets, json_file)
+tweets_list = search_tweets.get('statuses')
+for tweets in tweets_list:
+    print(tweets.get("text"))
 
